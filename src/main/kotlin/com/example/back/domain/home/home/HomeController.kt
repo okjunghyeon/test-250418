3552818702812f005e4ae0609d6c51e75a45df02
jeonghyeon-ohk <ok6737@naver.com>
2025-04-16 23:56:50 +0900

    work2

--- src/main/kotlin/com/example/back/domain/home/home/HomeController.kt
+++ src/main/kotlin/com/example/back/domain/home/home/HomeController.kt
@@ -1,9 +1,12 @@
 package com.example.back.domain.home.home
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
 @RestController
 class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping
     fun home(): String {
         return "Hello Kotlin!"
     }
